58f302de5b694e8f0e715160d9b299fa0aa53889
Sabrina lee <sabrinaleess6@gmail.com>
2023-06-13 14:58:33 +0800

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'json'
-require 'open-uri'
-require 'faker'
-
-url = 'https://tmdb.lewagon.com/movie/top_rated'
-movies_s = URI.open(url).read
-movies = JSON.parse(movies_s)['results'].sample(5)
-movies.each do |movie|
-  original_title = movie['original_title']
-  overviews = movie['overview']
-  poster = movie['poster_path']
-  image_url = "https://image.tmdb.org/t/p/w500#{poster}"
-  vote_average = movie['vote_average']
-
-  Movie.create(
-    title: original_title,
-    overview: overviews,
-    poster_url: image_url,
-    rating: vote_average
-  )
-end
